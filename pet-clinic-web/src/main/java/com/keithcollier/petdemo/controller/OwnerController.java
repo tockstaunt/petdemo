a6b3ef55a8317945b1eeab78044c2cb4fe1f3728
Keith Collier <34369272+tockstaunt@users.noreply.github.com>
2018-12-22 00:41:01 -0600

    added page of a list of owners

---
+++ pet-clinic-web/src/main/java/com/keithcollier/petdemo/controller/OwnerController.java
@@ -0,0 +1,17 @@
+package com.keithcollier.petdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+
+        @RequestMapping({"","/","/index","/index.html"})
+        public String listVets(){
+
+            return "owners/index";
+        }
+
+    }
