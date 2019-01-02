5c98adea8028cf3cc15b5b732fc1d2823ecca88b
Keith Collier <34369272+tockstaunt@users.noreply.github.com>
2019-01-02 00:01:02 -0600

    added thymeleaf code to show list of owners

--- pet-clinic-web/src/main/java/com/keithcollier/petdemo/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/keithcollier/petdemo/controller/OwnerController.java
@@ -1,16 +1,25 @@
 package com.keithcollier.petdemo.controller;
 
+import com.keithcollier.petdemo.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
 
-        @RequestMapping({"","/","/index","/index.html"})
-        public String listVets(){
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
+
+    @RequestMapping({"","/","/index","/index.html"})
+        public String listVets(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
             return "owners/index";
         }
 
