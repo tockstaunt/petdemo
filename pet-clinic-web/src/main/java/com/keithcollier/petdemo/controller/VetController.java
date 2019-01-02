4e6519d0c68b50256820db45c5ff7a975e09f221
Keith Collier <34369272+tockstaunt@users.noreply.github.com>
2019-01-02 00:10:55 -0600

    added thymeleaf code to show list of vets on vets.html

--- pet-clinic-web/src/main/java/com/keithcollier/petdemo/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/keithcollier/petdemo/controller/VetController.java
@@ -1,14 +1,23 @@
 package com.keithcollier.petdemo.controller;
 
+import com.keithcollier.petdemo.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index","vets/index.html"})
-    public String listVets(){
+    private final VetService vetService;
 
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"/vets","/vets/index","vets/index.html"})
+    public String listVets(Model model){
+
+        model.addAttribute("vet", vetService.findAll());
         return "vet/index";
     }
 
