d487750ce50c7b41935b2b9942eecd62c2dba4cf
Keith Collier <34369272+tockstaunt@users.noreply.github.com>
2019-01-04 04:46:52 -0600

    added contact info to owner model

---
+++ pet-clinic-data/src/main/java/com/keithcollier/petdemo/model/Specialties.java
@@ -0,0 +1,14 @@
+package com.keithcollier.petdemo.model;
+
+public class Specialties extends BaseEntity {
+
+    private String descption;
+
+    public String getDescption() {
+        return descption;
+    }
+
+    public void setDescption(String descption) {
+        this.descption = descption;
+    }
+}
--- pet-clinic-data/src/main/java/com/keithcollier/petdemo/model/Vet.java
+++ pet-clinic-data/src/main/java/com/keithcollier/petdemo/model/Vet.java
@@ -1,4 +1,16 @@
 package com.keithcollier.petdemo.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialties> specialties;
+
+    public Set<Specialties> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialties> specialties) {
+        this.specialties = specialties;
+    }
 }
